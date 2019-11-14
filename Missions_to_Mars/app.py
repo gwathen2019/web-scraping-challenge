bd28a261f265a94554986bd3705ed8869810a1ce
gwathen2019 <gwathen@att.net>
2019-11-14 13:51:30 -0600

    git commit

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_craigslist
+import scrape_mars
 
+#create an instance of Flask
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
@@ -11,18 +12,19 @@ mongo = PyMongo(app)
 # Or set inline
 # mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
 
-
+#Route to render index.html template using the data from Mongo
 @app.route("/")
-def index():
-    listings = mongo.db.listings.find_one()
-    return render_template("index.html", listings=listings)
+def home():
+    planet_data = mongo.db.collection.find_one()
+    return render_template("index.html", mars=planet_data)
 
-
+#Route that will trigger the scrape function
 @app.route("/scrape")
-def scraper():
-    listings = mongo.db.listings
-    listings_data = scrape_craigslist.scrape()
-    listings.update({}, listings_data, upsert=True)
+def scrape():
+    mars_data = scrape_mars.scrape_info()
+
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
     return redirect("/", code=302)
 
 
